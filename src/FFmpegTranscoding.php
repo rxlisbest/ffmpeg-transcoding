b396f7f2760f4bb147710c9d7b68708f7a3b0d28
Roy <rxlisbest@gmail.com>
2018-08-14 15:11:22 +0800

    composer init

---
+++ src/FFmpegTranscoding.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruixinglong
+ * Date: 2018/8/14
+ * Time: 下午3:09
+ */
+
+namespace Rxlisbest\FFmpegTranscoding;
+
+
+class FFmpegTranscoding
+{
+    
+}
